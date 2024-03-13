cbd251b9b0879e7e78cd53cb4a5a92846bc6be58
Dennis-Johnsen <jonhsendennis@gmail.com>
2024-03-13 14:05:39 +0100

    Initial commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SmartPack.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SmartPack.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
